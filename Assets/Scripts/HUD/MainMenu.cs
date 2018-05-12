db1b44251117d401548658681ad65ae7aa2105ab
Maksim <lamax_99@mail.ru>
2018-05-12 21:28:41 +0500

    +buttons and them handler

---
+++ Assets/Scripts/HUD/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadByIndex(int index)
+    {
+        Debug.Log("Load");
+        SceneManager.LoadScene(index);
+        Debug.Log("Loaded");
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
